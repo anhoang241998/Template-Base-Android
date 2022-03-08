b858459cb2331dd3e00e20e19ed8a9c44e7a6968
Nguyen Van Tan <doctorblue.king@gmail.com>
2022-03-08 14:12:47 +0700

    Remove unused resource

--- core/src/main/java/com/vn/rogo/base/core/utils/ThemeUtils.kt
+++ core/src/main/java/com/vn/rogo/base/core/utils/ThemeUtils.kt
@@ -1,10 +1,9 @@
 package com.vn.rogo.base.core.utils
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
 
 
-fun Context.setDarkModeTheme(isDarkMode: Boolean) {
+fun setDarkModeTheme(isDarkMode: Boolean) {
     if (isDarkMode) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     } else {
