4d35182e8f8186cbc265941a506766ebaf4c00ac
Nguyen Van Tan <doctorblue.king@gmail.com>
2022-03-08 13:24:45 +0700

    Fix save dark mode state

--- app/src/main/java/com/rogo/base/MyApplication.kt
+++ app/src/main/java/com/rogo/base/MyApplication.kt
@@ -1,6 +1,8 @@
 package com.rogo.base
 
 import android.app.Application
+import com.vn.rogo.base.core.utils.setDarkModeTheme
+import com.vn.rogo.data.data_source.preferences.ThemePreference
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -10,12 +12,12 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         //TODO: enable/disable dark theme
-//        ThemePreference.getThemePreferenceData(this, {
-//            // handle error
-//        }) {
-//            isDarkMode = it.isDarkMode
-//            setDarkModeTheme(it.isDarkMode)
-//        }
+        ThemePreference.getThemePreferenceData(this, {
+            // handle error
+        }) {
+            isDarkMode = it.isDarkMode
+            setDarkModeTheme(it.isDarkMode)
+        }
 
 //        if (BuildConfig.DEBUG) {
 //            Recovery.getInstance()
